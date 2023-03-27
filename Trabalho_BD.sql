cf183bcd5c645407407afe07ca5ad9e3c95d7baa
Breno Raeder <brenoraeder97@gmail.com>
2023-03-27 16:01:29 -0300

    Atualizando script

--- Trabalho_BD.sql
+++ Trabalho_BD.sql
@@ -1,52 +1,60 @@
-create table cliente(
-id integer primary key autoincrement not null,
-nome_completo text,
-nome_usuario text 
-email text,
-cpf integer,
-data_nascimento text,
-endereco text
-)
+--criação das tabelas (DDL)
 
-create table pedido (
-id integer primary key autoincrement not null,
-data_pedido  text,
-id_funcionario integer,
-id_cliente integer,
-foreign key(id_funcionario) references funcionario(id),
-foreign key(id_cliente) references cliente(id)
-)
+create table cliente(
+	id integer primary key autoincrement not null,
+	nome_completo text,
+	nome_usuario text,
+	cpf integer,
+	data_nascimento text,
+	endereco text
+);
+
+alter table cliente add column email text 
 
 create table funcionario(
-id integer primary key autoincrement not null,
-nome text,
-cpf integer
-)
+	id integer primary key autoincrement not null,
+	nome text,
+	cpf integer
+);
 
-create table produto(
-id integer primary key autoincrement not null,
-nome text,
-descricao text,
-quantidade_no_estoque integer,
-data_de_fabricacao text,
-valor_unitario integer,
-id_categoria integer,
-foreign key(id_categoria) references categoria(id)
-)
+create table pedido (
+	id integer primary key autoincrement not null,
+	data_pedido text,
+	id_funcionario integer,
+	id_cliente integer,
+	foreign key(id_funcionario) references funcionario(id),
+	foreign key(id_cliente) references cliente(id)
+);
 
 create table categoria (
-id integer primary key autoincrement not null,
-nome text,
-descricao text
-)
+	id integer primary key autoincrement not null,
+	nome text,
+	descricao text
+);
+
+create table produto(
+	id integer primary key autoincrement not null,
+	nome text,
+	descricao text,
+	quantidade_no_estoque integer,
+	data_de_fabricacao text,
+	valor_unitario integer,
+	id_categoria integer,
+	id_funcionario integer,
+	foreign key(id_categoria) references categoria(id),
+	foreign key(id_funcionario) references funcionario(id)
+);
 
 create table produto_pedido(
-id integer primary key autoincrement not null,
-id_produto integer,
-id_pedido integer,
-foreign key(id_produto) references produto(id),
-foreign key(id_pedido) references pedido(id)
-)
+	id integer primary key autoincrement not null,
+	id_produto integer,
+	id_pedido integer,
+	foreign key(id_produto) references produto(id),
+	foreign key(id_pedido) references pedido(id)
+);
+
+--inserção de dados nas tabelas (pelo menos 5 registros em cada uma) (DML)
+
 
 insert into cliente (nome_completo, nome_usuario, cpf, data_nascimento, endereco) values
 ('Nelson', '@nelson', 80215458810,'26/02/1997','Praça Pasteur'),
@@ -61,59 +69,60 @@ insert into funcionario (nome,cpf) values
 ('João',88888888888),
 ('Marcos',9999999999),
 ('Francisco',2222222222),
-('Carlos',5555555555555)
+('Carlos',5555555555555);
 
 
 insert into pedido (data_pedido,id_funcionario,id_cliente) values
-('26/02Q1997',1,1),
+('26/02/1997',1,1),
 ('07/05/2022',2,5),
 ('08/05/2015',5,3),
 ('06/07/2015',2,3),
-('07/05/2015',4,3)
+('07/05/2015',4,3);
 
-insert into produto (nome,descricao,quantidade_no_estoque,data_de_fabricacao,valor_unitario,id_categoria) values
-('Computador','Um belo computador',205,'14/02/1998',2500,1),
-('Navio','Um belo navio',3,'20/02/1998',250000,1),
-('Tesoura','Uma bela tesoura',5,'21/02/1998',2,1),
-('Pá','Uma bela páo',15,'25/02/1998',250,1)
-('Carro','Um belo carro',25,'26/02/1998',25000,1)
+insert into produto (nome,descricao,quantidade_no_estoque,data_de_fabricacao,valor_unitario,id_categoria,id_funcionario) values
+('Computador','Um belo computador',205,'14/02/1998',2500,1,2),
+('Navio','Um belo navio',3,'20/02/1998',250000,1,3),
+('Tesoura','Uma bela tesoura',5,'21/02/1998',2,1,1),
+('Pá','Uma bela páo',15,'25/02/1998',250,1,5),
+('Carro','Um belo carro',25,'26/02/1998',25000,1,4);
 
 insert into categoria (nome,descricao) values
 ('Veiculos','Carros e motos'),
 ('Ferramentas','Para obras'),
 ('Materias básicos','construção'),
 ('alimentos','revenda'),
-('computadores','estoque')
+('computadores','estoque');
 
 insert into produto_pedido (id_produto,id_pedido) values
 (1,6),
 (2,4),
 (3,2),
-(8,1)
-(1,5)
+(8,1),
+(1,5);
 
-
-alter table cliente add column email text 
+--atualização em algum registro em uma tabela (DML)
 
 update cliente 
 set email = 'nelson@outllool.com' 
-where id = 1
+where id = 1;
 
 update cliente 
 set email = 'pedro@outllool.com'
-where id = 3
+where id = 3;
 
 update cliente 
 set email = 'joao@outllool.com'
-where id = 4
+where id = 4;
 
 update cliente 
 set email = 'mario@outllool.com'
-where id = 5
+where id = 5;
 
 update cliente 
 set email = 'carlos@outllool.com'
-where id = 6
+where id = 6;
+
+--exclusão de algum registro em uma tabela (DML)
 
 delete from cliente 
 where id = 2
